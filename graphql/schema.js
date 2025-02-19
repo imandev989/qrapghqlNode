f1337f8016522891d2281076a81deca9a46b7ebf
itLinux <itlinux@itLinux.lightspeed.brhmal.sbcglobal.net>
2025-02-19 10:53:38 +0330

    create teacher query

--- graphql/schema.js
+++ graphql/schema.js
@@ -5,43 +5,62 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLList,
-} = require("graphql");
-
-const courses = [
+    GraphQLInt,
+  } = require("graphql");
+  
+  const courses = [
     { id: 1, title: "React.js", price: 4_800_000 },
     { id: 2, title: "Node.js", price: 3_300_000 },
     { id: 3, title: "Vue.js", price: 0 },
     { id: 4, title: "GraphQL", price: 970_000 },
-];
-
-
-
-const CourseType = new GraphQLObjectType({
+  ];
+  
+  const teachers = [
+    { id: 1, name: "Amin", age: 24 },
+    { id: 2, name: "Qadir", age: 22 },
+    { id: 3, name: "Ashkan", age: 28 },
+    { id: 4, name: "Moein", age: 22 },
+  ];
+  
+  const CourseType = new GraphQLObjectType({
     name: "Course",
     fields: () => ({
-        id: { type: GraphQLString },
-        title: { type: GraphQLString },
-        price: { GraphQLString }
-    })
-})
-
-
-const RootQuery = new GraphQLObjectType({
+      id: { type: GraphQLString },
+      title: { type: GraphQLString },
+      price: { type: GraphQLString },
+    }),
+  });
+  
+  const TeacherType = new GraphQLObjectType({
+    name: "Teacher",
+    fields: () => ({
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      age: { type: GraphQLInt },
+    }),
+  });
+  
+  const RootQuery = new GraphQLObjectType({
     name: "RootQuery",
     fields: {
-        courses: {
-            type: new GraphQLList(CourseType),
-            resolve: () => {
-                return courses;
-            }
-        }
-    }
-})
-
-
-const schema = new GraphQLSchema({
-    query: RootQuery
-})
-
-
-module.exports = schema;
+      courses: {
+        type: new GraphQLList(CourseType),
+        resolve: () => {
+          return courses;
+        },
+      },
+  
+      teachers: {
+        type: new GraphQLList(TeacherType),
+        resolve: () => {
+          return teachers;
+        },
+      },
+    },
+  });
+  
+  const schema = new GraphQLSchema({
+    query: RootQuery,
+  });
+  
+  module.exports = schema;
